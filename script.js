1530786f1d7c42abba3ec374393931b5daf5099f
sapatanne <aan333n3@gmail.com>
2025-08-28 22:45:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
-    botaoDeAcessibilidade.addEventListener('click', function () {
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-
-    })
+ 
+    botaoDeAcessibilidade.addEventListener('click', function (e) {
+        e.preventDefault();
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    });
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    let tamanhoAtualFonte = 1;
+    let tamanhoAtualFonte = 1; 
+    const tamanhoMinimo = 0.8;
+    const tamanhoMaximo = 2;
 
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    aumentaFonteBotao.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (tamanhoAtualFonte < tamanhoMaximo) {
+            tamanhoAtualFonte += 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte.toFixed(1)}rem`;
+        }
+    });
 
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-})
+    diminuiFonteBotao.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (tamanhoAtualFonte > tamanhoMinimo) {
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte.toFixed(1)}rem`;
+        }
+    });
+});
